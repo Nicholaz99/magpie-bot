dbd825465879629f3bc854cb826c1800aa43566d
Nicholaz99 <nicholasmagbanua@gmail.com>
2018-02-23 09:00:48 +0700

    fix port

--- magpie-bot.py
+++ magpie-bot.py
@@ -95,7 +95,7 @@ if __name__ == "__main__":
 	arg_parser = ArgumentParser(
 		usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
 	)
-	arg_parser.add_argument('-p', '--port', default=port_used, help='port')
+	arg_parser.add_argument('-p', '--port', default=process.env.PORT, help='port')
 	arg_parser.add_argument('-d', '--debug', default=False, help='debug')
 	options = arg_parser.parse_args()
 
